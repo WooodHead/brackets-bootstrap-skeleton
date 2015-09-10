427150f12090357615c8b3e1db4a681399f83727
Caleb Ely <le717.code@yahoo.com>
2015-09-09 22:49:41 -0400

    Fix #18

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 define(function (require, exports, module) {
   "use strict";
   /*
-   * Simple extension that adds a "File > New Bootstrap 3 Document" menu item
-   * to insert an Bootstrap 3 HTML "skeleton" at cursor position
+   * Simple extension that adds an "Edit > New Bootstrap Document" menu item
+   * to insert an Bootstrap HTML "skeleton" at cursor position
    */
   var AppInit            = brackets.getModule("utils/AppInit"),
       CommandManager     = brackets.getModule("command/CommandManager"),
@@ -17,7 +17,7 @@ define(function (require, exports, module) {
   var indentUnits  = "";
 
   // jQuery version constant
-  var JQUERY_VERSION = "1.11.1";
+  var JQUERY_VERSION = "1.11.3";
 
 
   /**
@@ -72,20 +72,17 @@ define(function (require, exports, module) {
    * Get the latest stable Boostrap version using the GitHub API
    */
   function _getLatestBootstrap() {
-    // However, we define a fallback just in case...
-    var fallbackVersion = "3.2.0";
+    // However, we define a fallback just in case
+    var fallbackVersion = "3.3.5";
     var result = new $.Deferred();
 
     // Perform an asynchronous request
     $.ajax({
       cache: true,
       dataType: "json",
-      url: "https://api.github.com/repos/twbs/bootstrap/releases",
+      url: "https://api.github.com/repos/twbs/bootstrap/releases/latest",
       success: function(data) {
-        // Do not accept prereleases
-        if (!data[0].prerelease) {
-          result.resolve(data[0].tag_name.replace(/^v/, ""));
-        }
+        result.resolve(data.tag_name.replace(/^v/, ""));
       },
       error: function() {
         result.resolve(fallbackVersion);
@@ -104,12 +101,12 @@ define(function (require, exports, module) {
     var htmlSkelly = "<!DOCTYPE html>\n<html lang=''>\n<head>\nindent-size<meta charset='UTF-8'>\n" +
         "indent-size<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n" +
         "indent-size<meta name='description' content=''>\nindent-size<meta name='author' content=''>\n" +
-        "indent-size<title>Starter Template for Bootstrap boots-version</title>\nindent-size<link rel='shortcut icon' href=''>\n" +
-        "indent-size<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/boots-version/css/bootstrap.min.css'>\n" +
-        "indent-size<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/boots-version/css/bootstrap-theme.min.css'>\n" +
+        "indent-size<title>Starter Template for Bootstrap {boots-version}</title>\nindent-size<link rel='shortcut icon' href=''>\n" +
+        "indent-size<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/{boots-version}/css/bootstrap.min.css'>\n" +
+        "indent-size<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/{boots-version}/css/bootstrap-theme.min.css'>\n" +
         "indent-size<style>body{padding-top:50px;}.starter-template{padding:40px 15px;text-align:center;}</style>\n\n" +
-        "indent-size<!--[if IE]>\nindent-sizeindent-size<script src='https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js'></script>\n" +
-        "indent-sizeindent-size<script src='https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js'></script>\nindent-size<![endif]-->\n" +
+        "indent-size<!--[if IE]>\nindent-sizeindent-size<script src='https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js'></script>\n" +
+        "indent-sizeindent-size<script src='https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js'></script>\nindent-size<![endif]-->\n" +
         "</head>\n\n<body>\nindent-size<nav class='navbar navbar-inverse navbar-fixed-top' role='navigation'>\n" +
         "indent-sizeindent-size<div class='container'>\nindent-sizeindent-sizeindent-size<div class='navbar-header'>\n" +
         "indent-sizeindent-sizeindent-sizeindent-size<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>\n" +
@@ -127,10 +124,10 @@ define(function (require, exports, module) {
         "indent-sizeindent-size</div>\nindent-size</nav>\n\n" +
         "indent-size<div class='container'>\nindent-sizeindent-size<div class='starter-template'>\n" +
         "indent-sizeindent-sizeindent-size<h1>Hello, world!</h1>\n" +
-        "indent-sizeindent-sizeindent-sizeindent-size<p class='lead'>Now you can start your own project with <a target='_blank' href='http://getbootstrap.com/'>Bootstrap boots-version</a>. This plugin is a fork from <a href='https://github.com/le717/brackets-html-skeleton#readme'>HTML Skeleton</a>.</p>\n" +
+        "indent-sizeindent-sizeindent-sizeindent-size<p class='lead'>Now you can start your own project with <a target='_blank' href='http://getbootstrap.com/'>Bootstrap {boots-version}</a>. This plugin is a fork from <a href='https://github.com/le717/brackets-html-skeleton#readme'>HTML Skeleton</a>.</p>\n" +
         "indent-sizeindent-size</div>\nindent-size</div>\n\n" +
-        "indent-size<script src='https://ajax.googleapis.com/ajax/libs/jquery/jq-version/jquery.min.js'></script>\n" +
-        "indent-size<script src='https://maxcdn.bootstrapcdn.com/bootstrap/boots-version/js/bootstrap.min.js'></script>\n" +
+        "indent-size<script src='https://ajax.googleapis.com/ajax/libs/jquery/{jq-version}/jquery.min.js'></script>\n" +
+        "indent-size<script src='https://maxcdn.bootstrapcdn.com/bootstrap/{boots-version}/js/bootstrap.min.js'></script>\n" +
         "</body>\n</html>\n";
 
     // Since fetching the lastest version is an async process,
@@ -145,8 +142,8 @@ define(function (require, exports, module) {
           // and replace them with the appropriate version number,
           // as well as for the `indent-size` keyword with indentation settings
           // Also replace all single quotes with double quotes
-          htmlSkelly = htmlSkelly.replace(/boots-version/g, version)
-                                 .replace(/jq-version/g, JQUERY_VERSION)
+          htmlSkelly = htmlSkelly.replace(/{boots-version}/g, version)
+                                 .replace(/{jq-version}/g, JQUERY_VERSION)
                                  .replace(/indent-size/g, indentUnits)
                                  .replace(/'/g, "\"");
           editor.document.replaceRange(htmlSkelly, insertionPos);
@@ -162,8 +159,8 @@ define(function (require, exports, module) {
    */
   AppInit.appReady(function () {
     var EXTENSION_ID = "mirorauhala.bootstrap-skeleton";
-    CommandManager.register("New Bootstrap 3 Document", EXTENSION_ID, _inserthtmlSkelly);
-    var theMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
+    CommandManager.register("New Bootstrap Document", EXTENSION_ID, _inserthtmlSkelly);
+    var theMenu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
     theMenu.addMenuItem(EXTENSION_ID);
     _setIndentSize();
   });
